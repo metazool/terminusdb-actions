d36cc00169e92cf01b94e085af2d1fbc3eee2c29
Jo Walsh <jowalsh@bgs.ac.uk>
2020-11-02 20:10:05 +0000

    add failing test for user auth with python client

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,2 +1,13 @@
-def test_graph():
-    pass
+import pytest
+from terminusdb_client import WOQLClient
+
+HOST = 'http://localhost:6363'
+
+@pytest.fixture
+def client():
+    client = WOQLClient('http://localhost:6363')
+    client.basic_auth("root", "admin")
+    return client
+
+def test_create(client):
+    client.create_database('test')

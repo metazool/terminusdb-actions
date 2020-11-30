292f39871fad6f0dadc472de5adeaa08d5c55c06
Jo Walsh <jowalsh@bgs.ac.uk>
2020-11-30 17:34:55 +0000

    insert some ttl, but nothing back on query

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,15 +1,33 @@
 import pytest
+from rdflib import Graph
 from terminusdb_client import WOQLClient
+from terminusdb_client import WOQLQuery as WQ
+from terminusdb_client.woqlclient.errors import APIError
 
 HOST = 'http://localhost:6363'
+
 
 @pytest.fixture
 def client():
     client = WOQLClient(HOST)
-#    client.organization("admin")
-    client.basic_auth("admin", "root")
-    client.connect(key="root", account="admin", user="admin")
+    client.connect(key="root", account="admin", user="admin", db="test")
     return client
 
+
 def test_create(client):
-    client.create_database('test', accountid="admin")
+    try:
+        client.create_database('test', accountid="admin")
+    except APIError as err:
+        print(err.msg)
+    with pytest.raises(APIError):
+        client.create_database('test', accountid="admin")
+
+
+def test_insert_file(client):
+    g = Graph()
+    g.parse('./data/lexicon-named-rock-unit.nt', format='nt')
+    ttl = str(g.serialize(format='ttl'))
+#    print(ttl)
+    client.insert_triples("instance", "main", ttl, "ttl insert")
+    result = WQ().limit(4).triple("v:X", "v:P", "v:Y").execute(client)
+    print(result)
